db55995de426904330a7528d06de4af3859e2f46
gustavoduqueo <gustavoduqueo@gmail.com>
2024-04-26 23:48:44 -0500

    Wordpress

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -6,8 +6,3 @@ output "ec2_wordpress_subnet_id" {
   description = "Subnet ID"
   value       = aws_subnet.subnet_itm_wordpress_1.id
 }
-
-output "securitygroup_id" {
-  description = "Security Group ID"
-  value       = aws_security_group.sgr_itm_wordpress.id
-}
